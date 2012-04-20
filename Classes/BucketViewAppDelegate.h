f307eb445f8002329d55f93b75d3ce8c12330d71
Tony Eichelberger <tony.eich@gmail.com>
2012-04-19 20:38:31 -0500

    stuff in progress for recipes book

--- Classes/BucketViewAppDelegate.h
+++ Classes/BucketViewAppDelegate.h
@@ -3,7 +3,6 @@
 //  BucketView
 //
 //  Created by Tony Eichelberger on 7/3/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
 //
 
 #import "SplashScreenView.h"
--- SplashScreenView.h
+++ SplashScreenView.h
@@ -3,7 +3,6 @@
 //  BucketView
 //
 //  Created by Tony on 12/20/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
